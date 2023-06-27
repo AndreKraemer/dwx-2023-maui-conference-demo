961a3176a08866be1100edfdd72474b09a9b002b
André Krämer <andre.kraemer@qualitybytes.de>
2023-06-27 09:42:18 +0200

    Projektdateien hinzufügen.

---
+++ ConferenceMauiDemo/MauiProgram.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Maps;
+using CommunityToolkit.Maui.Markup;
+using ConferenceMauiDemo.Services;
+using ConferenceMauiDemo.ViewModels;
+using ConferenceMauiDemo.Views;
+using Microsoft.Extensions.Logging;
+
+namespace ConferenceMauiDemo;
+
+public static class MauiProgram
+{
+    private const string bingMapsKey = "<Insert your Bing Maps Key here>";
+
+    public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiMaps()
+			.UseMauiCommunityToolkit()
+			.UseMauiCommunityToolkitMarkup()
+            .UseMauiCommunityToolkitMaps(bingMapsKey)
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Font Awesome 6 Free-Solid-900.otf", "FA-6-Solid-900");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+        builder.Services.AddSingleton<IDataService, FileDataService>();
+        builder.Services.AddSingleton<IExportService, CsvExportService>();
+        builder.Services.AddSingleton<IDialogService, DialogService>();
+        builder.Services.AddTransient<MainPage, MainPageViewModel>();
+        builder.Services.AddTransient<SessionsPage, SessionsPageViewModel>();
+		builder.Services.AddTransient<SessionsPageMarkup>();
+        builder.Services.AddTransientWithShellRoute<SessionDetailPage, SessionDetailPageViewModel>(nameof(SessionDetailPage));
+        builder.Services.AddTransient<DependencyInjectionDemoPage>();
+        builder.Services.RegisterPlatformServices();
+
+        return builder.Build();
+	}
+}
---
+++ ConferenceMauiDemo/Platforms/Windows/ServiceCollectionRegistrationExtension.cs
@@ -0,0 +1,15 @@
+﻿using ConferenceMauiDemo.Platforms.Windows;
+using ConferenceMauiDemo.Services;
+using ConferenceMauiDemo.WinUI;
+
+namespace ConferenceMauiDemo
+{
+    public static class ServiceCollectionRegistrationExtension
+    {
+        public static void RegisterPlatformServices(this IServiceCollection services)
+        {
+            services.AddTransient<IDeviceInformation, WindowsDeviceInformation>();
+            services.AddTransient<IExportService, WordExportService>();
+        }
+    }
+}
